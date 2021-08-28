83fed1a82e5f9ce418ce7ed5d8e8934a8a2f267c
Cesar Gomez <cesar.gomezb@crushbank.com>
2021-08-28 11:45:17 -0500

    feat: extracted ExpenseItem component into a separed component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -11,11 +11,7 @@ function App() {
   return (
     <div>
       <h2>Expenses</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
