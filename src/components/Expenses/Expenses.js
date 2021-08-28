c27f48d8e27788e969b219ab610093504f635f8b
Cesar Gomez <cesar.gomezb@crushbank.com>
2021-08-28 18:20:59 -0500

    feat: add an expense and filter data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  }
+
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  })
+
   return (
     <Card className="expenses">
-      {props.expenses.map(({ id, title, amount, date }) => (
+      <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler}/>
+      {filteredExpenses.map(({ id, title, amount, date }) => (
         <ExpenseItem key={id} title={title} amount={amount} date={date} />
       ))}
     </Card>
