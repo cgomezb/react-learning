8a04de5a3ea02e941761ca3e38f9c88b284af427
Cesar Gomez <cesar.gomezb@crushbank.com>
2021-08-29 09:25:20 -0500

    refactor: create a new component for expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -9,18 +9,16 @@ function Expenses(props) {
 
   const filterChangeHandler = (selectedYear) => {
     setSelectedYear(selectedYear);
-  }
+  };
 
   const filteredExpenses = props.expenses.filter(expense => {
     return expense.date.getFullYear().toString() === selectedYear;
-  })
+  });
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler}/>
-      {filteredExpenses.map(({ id, title, amount, date }) => (
-        <ExpenseItem key={id} title={title} amount={amount} date={date} />
-      ))}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,18 @@
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.expenses.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.expenses.map(({ id, title, amount, date }) => (
+        <ExpenseItem key={id} title={title} amount={amount} date={date} />
+      ))}
+    </ul>
+  )
+}
+
+export default ExpensesList;
